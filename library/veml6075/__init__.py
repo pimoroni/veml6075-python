9450be67396c07e5791e58e5141738e9572b838f
Gee Bartlett <geeus81@gmail.com>
2019-06-03 14:54:39 +0000

    update

--- library/veml6075/__init__.py
+++ library/veml6075/__init__.py
@@ -1,7 +1,6 @@
 import time
 from i2cdevice import Device, Register, BitField
 from i2cdevice.adapter import Adapter, LookupAdapter, U16ByteSwapAdapter
-import datetime
 
 __version__ = '0.0.1'
 
@@ -20,6 +19,7 @@ class SensorDataAdapter(Adapter):
         # print (bin(MSB),bin(LSB))
         return MSB + LSB
 
+
 class BCDAdapter(Adapter):
 
     def _decode(self, value):
@@ -30,9 +30,10 @@ class BCDAdapter(Adapter):
 
     def _encode(self, value):
         upper = (int(value / 10)) << 4
-        lower = value % 10 
+        lower = value % 10
 
-        return upper | lower 
+        return upper | lower
+
 
 class InterruptLookupAdapter(Adapter):
     """Special version of the
@@ -63,53 +64,119 @@ class InterruptLookupAdapter(Adapter):
 
         return return_value
 
+
 class VEML6075:
-    def __init__(self, i2c_addr=0x26, i2c_dev=None):
+    def __init__(self, i2c_addr=0x10, i2c_dev=None):
         self._i2c_addr = i2c_addr
         self._i2c_dev = i2c_dev
         self._is_setup = False
         # Device definition
-        self._veml6075 = Device([0x52], i2c_dev=self._i2c_dev, bit_width=16, registers=(
+        self._veml6075 = Device([0x10], i2c_dev=self._i2c_dev, bit_width=8, registers=(
             Register('UV_CONF', 0x00, fields=(
-                BitField('uv_intergration_time', 0x011100000000000000, adapter=LookupAdapter({
+                BitField('value', 0xFFFF),
+                BitField('uv_intergration_time', 0b0111000000000000, adapter=LookupAdapter({
                     '50ms': 0b000,
                     '100ms': 0b001,
                     '200ms': 0b010,
                     '400ms': 0b011,
                     '800ms': 0b100
-                    })),
-                BitField('high_dynamic_enable', 0x000010000000000000),
-                BitField('trigger_measurement', 0x000001000000000000),
-                BitField('enable_trigger_mode', 0x000000100000000000),
-                BitField('shutdown', 0x000000010000000000)
-            )),
+                })),
+                BitField('high_dynamic_enable', 0b0001000000000000),
+                BitField('trigger_measurement', 0b0000010000000000),
+                BitField('enable_trigger_mode', 0b0000001000000000),
+                BitField('shutdown', 0b0000000100000000)
+            ), bit_width=16),
             Register('UVA_DATA', 0x07, fields=(
-                BitField('data', 0xFFF, adapter=U16ByteSwapAdapter())
-            )),
-            Register('UVB_DATA', 0x07, fields=(
-                BitField('data', 0xFFF, adapter=U16ByteSwapAdapter())
-            )),
-            Register('UVCOMP1_DATA', 0x07, fields=(
-                BitField('data', 0xFFF, adapter=U16ByteSwapAdapter())
-            )),
-            Register('UVCOMP2_DATA', 0x07, fields=(
-                BitField('data', 0xFFF, adapter=U16ByteSwapAdapter())
-            )),
-            Register('ID', 0x07, fields=(
-                BitField('device_id', 0xFF)
-                
-            )),
+                BitField('data', 0xFFFF, adapter=U16ByteSwapAdapter() ),
+            ), bit_width=16),
+            Register('UVB_DATA', 0x09, fields=(
+                BitField('data', 0xFFFF, adapter=U16ByteSwapAdapter()),
+            ), bit_width=16),
+            Register('UVCOMP1_DATA', 0x0A, fields=(
+                BitField('data', 0xFFFF, adapter=U16ByteSwapAdapter()),
+            ), bit_width=16),
+            Register('UVCOMP2_DATA', 0x0B, fields=(
+                BitField('data', 0xFFFF, adapter=U16ByteSwapAdapter()),
+            ), bit_width=16),
+            Register('ID', 0x0C, fields=(
+                BitField('device_id', 0xFFFF, adapter=U16ByteSwapAdapter()),
+            ), bit_width=16),
+
+        ))
+
+    def get_intergration_time(self):
+
+        return self._veml6075.UV_CONF.get_uv_intergration_time()
+
+    def set_intergration_time(self, value):
+        try:
+            self._veml6075.UV_CONF.set_uv_intergration_time(value)
+        except RuntimeError:
+            raise RuntimeError(' {0} is and invalid setting for UV intergration time'.format(value))
+
+    def get_id(self):
+
+        return self._veml6075.ID.get_device_id() 
+
+    def get_shutdown(self):
+
+        return self._veml6075.UV_CONF.get_shutdown()
+
+    def set_shutdown(self, value):
+        self._veml6075.UV_CONF.set_shutdown(value)
+
+    def enable_high_dynamic_range(self):
+        self._veml6075.UV_CONF.set_high_dynamic_enable(1)
+
+    def disable_high_dynamic_range(self):
+        self._veml6075.UV_CONF.set_high_dynamic_enable(0)
+
+    def get_measurements(self):
+        return self._veml6075.UVA_DATA.get_data() , self._veml6075.UVB_DATA.get_data()
+
+    def get_commparitor_readings(self):
+
+        return self._veml6075.UVCOMP1_DATA.get_data() , self._veml6075.UVCOMP2_DATA.get_data()
+
+    def convert_to_index(self, uva, uvb, uv_comp1, uv_comp2):
+        result = 0 
+        #these values can be adusted for calibration 
+        uva_calib = 1
+        uvb_calib = 1
+        uv_comp1_calib = 1
+        uv_comp2_calib = 1
+        '''coefficients for open air sensor and thin teflon defuser up to 0.25mm 
+        For diffusor thickness of 0.4 mm and 0.7 mm other / lower IR coefficients need to be used. These are: uva_b_coef = 1.17 and uvb_d_coef = 1.58.
+        The visible cancellation coefficients stay the same.''' 
+        uva_a_coef = 2.22
+        uva_b_coef = 1.33
+        uvb_c_coef = 2.95
+        uvb_d_coef = 1.74
+        uva_responce = 0.001461
+        uvb_responce = 0.002591
+        uva_calc =  uva - ((uva_a_coef * uva_calib * uv_comp1) / uv_comp1_calib) - ((uva_b_coef * uva_calib * uv_comp2) / uv_comp2_calib)
+        uvb_calc =  uvb - ((uvb_c_coef * uvb_calib * uv_comp1) / uv_comp1_calib) - ((uvb_d_coef * uvb_calib * uv_comp2) / uv_comp2_calib)
+        uva_index = uva_calc * ( 1 / uva_calib ) * uva_responce
+        uvb_index = uvb_calc * ( 1 / uvb_calib ) * uvb_responce
+        result = (uva_index + uvb_index) / 2
+
+        return uva_index, uvb_index, result
 
 
-            
-            ))
+if __name__ == "__main__":
+    import smbus
 
-'''
-a = uva_a_coef = 2.22, which is the default value for the UVA VIS coefficient
-b = uva_b_coef = 1.33, which is the default value for the UVA IR coefficient
-c = uvb_c_coef = 2.95, which is the default value for the UVB VIS coefficient
-d = uvb_d_coef = 1.74, which is the default value for the UVB IR coefficient
-'''
+    bus = smbus.SMBus(1)
 
+    uv_sensor = VEML6075(i2c_dev=bus)
+    uv_sensor.set_shutdown(False)
+    uv_sensor.disable_high_dynamic_range()
 
-        
+    while 1: 
+        uva , uvb = uv_sensor.get_measurements()
+        uv_comp1, uv_comp2 = uv_sensor.get_commparitor_readings()
+        uv_sensor.set_intergration_time('100ms')
+        print( ' UVA : {0} UVB : {1} COMP 1 : {2} COMP 2 : {3}'.format(uva, uvb, uv_comp1, uv_comp2))
+        print ( 'UVA INDEX: {0[0]} UVB INDEX : {0[1]} AVG UV INDEX : {0[1]}'.format(uv_sensor.convert_to_index(uva, uvb, uv_comp1, uv_comp2)))
+
+        time.sleep(0.2)
