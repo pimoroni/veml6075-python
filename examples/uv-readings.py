7f6043eb7cc52133ff971c2cce03ca55fa0a9343
Gee Bartlett <geeus81@gmail.com>
2019-06-04 13:55:29 +0000

    added example description

--- examples/uv-readings.py
+++ examples/uv-readings.py
@@ -2,6 +2,8 @@ import time
 import veml6075
 import smbus
 
+print("""uv_readings.py - Starts up sensor and displays fresh reedings every second
+    Press Ctrl+C to exit.""")
 bus = smbus.SMBus(1)
 
 uv_sensor = veml6075.VEML6075(i2c_dev=bus)
@@ -16,4 +18,4 @@ while 1:
     print('UVA : {0} UVB : {1} COMP 1 : {2} COMP 2 : {3}'.format(uva, uvb, uv_comp1, uv_comp2))
     print ('UVA INDEX: {0[0]} UVB INDEX : {0[1]} AVG UV INDEX : {0[2]}'.format(uv_sensor.convert_to_index(uva, uvb, uv_comp1, uv_comp2)))
 
-    time.sleep(0.2)
+    time.sleep(1)
