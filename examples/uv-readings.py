c1008416cc31d9187acd37e25f54a26e8e9a97d6
Sandy Macdonald <sandyjmacdonald@gmail.com>
2019-06-10 13:09:39 +0100

    Little tweaks to example

--- examples/uv-readings.py
+++ examples/uv-readings.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import time
 import veml6075
 import smbus
@@ -6,6 +8,7 @@ print("""uv_readings.py - Starts up sensor and displays fresh readings every sec
     Press Ctrl+C to exit.""")
 bus = smbus.SMBus(1)
 
+# Create VEML6075 instance and set up
 uv_sensor = veml6075.VEML6075(i2c_dev=bus)
 uv_sensor.set_shutdown(False)
 uv_sensor.set_high_dynamic_range(False)
@@ -14,8 +17,9 @@ uv_sensor.set_integration_time('100ms')
 while 1:
     uva, uvb = uv_sensor.get_measurements()
     uv_comp1, uv_comp2 = uv_sensor.get_comparitor_readings()
+    uv_indices = uv_sensor.convert_to_index(uva, uvb, uv_comp1, uv_comp2)
 
     print('UVA : {0} UVB : {1} COMP 1 : {2} COMP 2 : {3}'.format(uva, uvb, uv_comp1, uv_comp2))
-    print ('UVA INDEX: {0[0]} UVB INDEX : {0[1]} AVG UV INDEX : {0[2]}'.format(uv_sensor.convert_to_index(uva, uvb, uv_comp1, uv_comp2)))
+    print ('UVA INDEX: {0[0]} UVB INDEX : {0[1]} AVG UV INDEX : {0[2]}\n'.format(uv_indices))
 
     time.sleep(0.2)
